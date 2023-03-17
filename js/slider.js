6d30188c2ccfde0978ef08350c95f2feb1f993d8
iamapusaha <apusaha392@gmail.com>
2023-03-17 14:34:49 +0600

    added js function and prokect done

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,21 @@
-console.log('loged')
+const images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg',
+]
+let imageIndx = 0;
+const imgElement = document.getElementById('img-slider');
+setInterval(() => {
+    if (imageIndx === images.length) {
+        imageIndx = 0;
+    }
+    const imageUrl = images[imageIndx]
+    imgElement.setAttribute('src', imageUrl)
+    imageIndx++;
+}, 2000)
